93dd22542f60fabd425aa56b308dbf7280c9e432
sliver <125364267+slivermarkr@users.noreply.github.com>
2024-01-13 15:05:32 +0800

    remove array elements via function parameters

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(myArray) {
+    
+    let newArray = [];
+    for(let i = 0; i < myArray.length; i++){
 
+        if(arguments[1] !== myArray[i] && arguments[2] !== myArray[i] && arguments [3] !== myArray[i] && arguments [4] !== myArray[i]){ 
+            newArray.push(myArray[i]);
+        }
+
+    }
+    return newArray;
 };
 
 // Do not edit below this line
