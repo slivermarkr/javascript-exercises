e18b2253cdda36d6a41e907f28ab12331dabac73
sliver <125364267+slivermarkr@users.noreply.github.com>
2024-01-13 13:23:33 +0800

    reversedString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(input) {
+let string = input;
+let stringReversed = "";
 
+for(let i = string.length - 1; i >= 0; i--){
+    stringReversed += string[i];
+}
+
+return stringReversed;
 };
 
 // Do not edit below this line
