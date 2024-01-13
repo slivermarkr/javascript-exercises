ea3eee9ae3f4ac463e1dac8b14ec20340c53e220
sliver <125364267+slivermarkr@users.noreply.github.com>
2024-01-13 13:01:22 +0800

    test 2 completed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,17 @@
-const repeatString = function() {
-
+const repeatString = function(inputString, numTimes) {
+    let string = inputString;
+    let times = 1;
+    if(numTimes < 0){
+        return "ERROR";
+    } else if (numTimes === 0){
+        return "";
+    }
+    while(times < numTimes){
+        string += inputString;
+        times++;
+    }
+    
+    return string;
 };
 
 // Do not edit below this line
