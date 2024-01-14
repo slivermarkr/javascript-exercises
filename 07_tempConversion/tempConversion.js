633a646f599ecd7d2941d5380619decd8aaf048b
sliver <125364267+slivermarkr@users.noreply.github.com>
2024-01-14 10:26:14 +0800

    celcius and fahrenheit

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,14 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(input) {
+let celcius = +((input - 32)/1.8).toFixed(1);
+  if(celcius === 0){
+    return celcius;
+  } 
+  return celcius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(input) {
+let faren = +((input*(9/5))+32).toFixed(1);
+ return faren;
 };
 
 // Do not edit below this line
