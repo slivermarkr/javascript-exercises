db530efd53c9e6adb2528300cb869d078fd88997
sliver <125364267+slivermarkr@users.noreply.github.com>
2024-01-20 17:44:44 +0800

    factoriaal function now working properly

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -26,8 +26,12 @@ const power = function(x, y) {
 	return x ** y;
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+  let result = 1;
+  for(let i = 1; i <= x; i++){
+    result *= i
+  }
+  return result;
 };
 
 // Do not edit below this line
