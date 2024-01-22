64a3e5d207df449f747701879674c45cf92226a9
sliver <125364267+slivermarkr@users.noreply.github.com>
2024-01-22 15:22:47 +0800

    fibonacci i do not know how i made this work but it works lol

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
+// return the equivalent member of the fibonacci sequence based on input
+//
 
+
+
+const fibonacci = function(input) {
+    if(input < 0)return "OOPS";
+    if(input === 0 || input === '0') return 0;
+
+    input = +input;
+    let fib =[1,1];
+    for(let i = fib.length; i <= input; i++) {
+        fib[i] = fib[i-1] + fib[i-2];
+    }
+    return fib[input-1];
 };
 
 // Do not edit below this line
