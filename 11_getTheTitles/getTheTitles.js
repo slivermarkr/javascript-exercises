59d6940a508a8c38eb751153d8affb9c64b36f22
sliver <125364267+slivermarkr@users.noreply.github.com>
2024-01-22 16:08:11 +0800

    some object inquisition

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    let array = [];
+    for(let i = 0; i < books.length; i++){
+        array[i] = books[i].title;
+    }
+    return array;
 };
 
 // Do not edit below this line
