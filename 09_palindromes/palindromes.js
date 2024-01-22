3fcea3c2bfa168c61ca08786b67004890c235533
sliver <125364267+slivermarkr@users.noreply.github.com>
2024-01-22 14:31:32 +0800

    palindrome

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,18 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let newStr = "";
+    let itsPalindrome = false;
+    for(let i = str.length - 1; i >= 0; i--){
+        newStr += str[i];
+    }
+    //exclude space and non alphabetic char
+    //then compare two string
+    newStr = newStr.replace(/[^a-z0-9]/gi, '').toLowerCase();
+    let modifiedStr = str.replace(/[^a-z0-9]/gi, '').toLowerCase();
+    
+    if(newStr === modifiedStr){
+        itsPalindrome = true;
+    }
+    return itsPalindrome;
 };
 
 // Do not edit below this line
