1961a662e50dc22835c81910bc5c561bd8c5aa67
sliver <125364267+slivermarkr@users.noreply.github.com>
2024-01-24 13:59:00 +0800

    tests twelve

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,19 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    const oldest = people
+        .sort((a, b) => {
+        const post = getAge(a.yearOfBirth,a.yearOfDeath);
+        const pre = getAge(b.yearOfBirth, b.yearOfDeath);
+        return post > pre ? 1 : -1;
+    })
+    return oldest[0];
 };
+
+const getAge = (birth,death) => {
+    if(!death){
+        death = new Date().getFullYear();
+    }
+    return birth - death;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
